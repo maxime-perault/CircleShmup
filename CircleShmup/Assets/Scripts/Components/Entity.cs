74de34f837b3c91aa904086cc7b7140be634d47b
Aredhele <vincentstehly@hotmail.fr>
2017-12-24 15:15:22 +0100

    Add base sorting layers (waiting for GDD) - Add empty entity component (waiting for GDD) - Add empty enemy scene (waiting for GDD)

---
+++ CircleShmup/Assets/Scripts/Components/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * TODO
+ */
+public class Entity : MonoBehaviour
+{
+    [SerializeField] protected int hitPoint;
+
+    /**
+     * TODO
+     */
+}
