d63bb8782ef3dabeb17e63acc9b1d16c6a02a1af
maxime-perault <maxime.perault.auditeur@lecnam.net>
2018-01-03 18:25:27 +0100

    Add Main menu buttons press A or space while on New Game to load asynchronously the game

---
+++ CircleShmup/Assets/Scripts/Menu/SelectMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System;
+
+public class SelectMenu : MonoBehaviour
+{
+    void Start ()
+    {
+    }
+	
+	void Update ()
+    {
+
+    }
+
+    public void NewGame()
+    {
+        StartCoroutine(LoadYourAsyncScene("MainGame"));
+    }
+
+    /*
+    ** Load the scene and wait with a yield until its done.
+    */
+    IEnumerator LoadYourAsyncScene(string name)
+    {
+        string path = "Scenes/"; path += name;
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(path);
+        
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
---
+++ CircleShmup/Assets/Scripts/Menu/SpaceShip.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour
+{
+    private float translation = 0;
+    private Vector2 pos;
+    private int actual_button = 0;
+    private bool isMoving = false;
+    private SelectMenu MenuClass;
+
+
+    public int nb_buttons = 4;
+    public GameObject SelectionObject;
+
+    void Start ()
+    {
+        pos.x = GetComponent<RectTransform>().localPosition.x;
+        pos.y = GetComponent<RectTransform>().localPosition.y;
+        MenuClass = SelectionObject.GetComponent<SelectMenu>();
+    }
+	
+	void Update ()
+    {
+        translation = Input.GetAxis("Vertical");
+
+        /*
+        ** SpaceShip shots
+        */
+        if ((Input.GetKey(KeyCode.Space) || Input.GetKey("joystick button 0"))
+            && (actual_button == 0))
+        {
+            MenuClass.NewGame();
+        }
+
+        /*
+        ** Wait until the stick return before moving again
+        */
+        if ((translation == 0) && (isMoving == true))
+            isMoving = false;
+
+        /*
+        ** Move the ship across the buttons
+        */
+        if (isMoving == false)
+        {
+            if ((translation < 0) && (actual_button < (nb_buttons - 1)))
+            {
+                ++actual_button;
+                pos.y -= 100;
+                GetComponent<RectTransform>().localPosition = new Vector3(pos.x, pos.y, 0);
+                isMoving = true;
+            }
+            else if ((translation > 0) && (actual_button > 0))
+            {
+                --actual_button;
+                pos.y += 100;
+                GetComponent<RectTransform>().localPosition = new Vector3(pos.x, pos.y, 0);
+                isMoving = true;
+            }
+        }
+    }
+}
