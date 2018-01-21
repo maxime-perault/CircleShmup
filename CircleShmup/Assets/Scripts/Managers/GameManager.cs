551a581261fa0f4ceaffc2c6050681e7d2dd5e07
peraul_m <maxime.perault.auditeur@lecnam.net>
2018-01-22 00:09:10 +0100

    add Highscore System still have to implement score at the end and a file save system

--- CircleShmup/Assets/Scripts/Managers/GameManager.cs
+++ CircleShmup/Assets/Scripts/Managers/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 /**
  * Main game manager, shares data between scenes
@@ -8,11 +9,19 @@ using System.Collections.Generic;
  * This class is a singleton non destroyable on LoadScene
  * @class GameManager
  */
+
+public struct ScoreBoard
+{
+    public string  name;
+    public int     score;
+};
+
 public class GameManager : MonoBehaviour
 {
     private static GameManager  SingletonRef;
 
     public string[] inputs;
+    public ScoreBoard[] scoreboard;
     public int      invertYaxis = 1;
 
     public enum e_input
@@ -43,6 +52,20 @@ public class GameManager : MonoBehaviour
     void Start ()
     {
         inputs = new string[System.Enum.GetNames(typeof(e_input)).Length];
+        scoreboard = new ScoreBoard[99];
+
+        for (int i = 0; i < 99; i++)
+        {
+            scoreboard[i].name = "NAMENAME";
+            scoreboard[i].score = 0;
+        }
+        scoreboard[97].score = 97;
+        scoreboard[38].score = 38;
+        scoreboard[22].score = 22;
+        scoreboard[21].score = 21;
+        scoreboard[12].score = 12;
+        scoreboard[11].score = 12;
+        Array.Sort<ScoreBoard>(scoreboard, (x, y) => y.score.CompareTo(x.score));
 
         inputs[(int)e_input.TURNLEFT] = "Mouse0";
         inputs[(int)e_input.TURNRIGHT] = "Mouse1";
--- CircleShmup/Assets/Scripts/Menu/HighScore/SelectScore.cs
+++ CircleShmup/Assets/Scripts/Menu/HighScore/SelectScore.cs
@@ -6,20 +6,89 @@ using UnityEngine.UI;
 public class SelectScore : ASelect
 {
     private GameManager manager;
+    private Scrollbar   scrollbar;
+    private Text        scoreText;
+    private Text        firstScore;
+    private Text        secondScore;
+    private Text        thirdScore;
 
     private new void Start()
     {
         base.Start();
         manager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        scrollbar = GameObject.Find("Scrollbar").GetComponent<Scrollbar>();
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+
+        firstScore = GameObject.Find("FirstPlace").GetComponent<Text>();
+        secondScore = GameObject.Find("SecondPlace").GetComponent<Text>();
+        thirdScore = GameObject.Find("ThirdPlace").GetComponent<Text>();
+
+        string fullBoard = "";
+
+        firstScore.text = getPos(1) + " " + manager.scoreboard[0].name + " " + getScore(manager.scoreboard[0].score) + "\n";
+        secondScore.text = getPos(2) + " " + manager.scoreboard[1].name + " " + getScore(manager.scoreboard[1].score) + "\n";
+        thirdScore.text = getPos(3) + " " + manager.scoreboard[2].name + " " + getScore(manager.scoreboard[2].score) + "\n";
+        for (int i = 3; i < manager.scoreboard.Length; i++)
+        {
+            if (i < 99)
+                fullBoard += getPos(i + 1) + " " + manager.scoreboard[i].name + " " +  getScore(manager.scoreboard[i].score) + "\n";
+        }
+        scoreText.text = fullBoard;
+    }
+
+    string  getPos(int nb)
+    {
+        if (nb < 10)
+            return ("0" + nb.ToString() + "_");
+        return (nb.ToString() + "_");
+
+    }
+
+    string getScore(float nb)
+    {
+        if (nb > 99999)
+        {
+            nb /= 1000;
+            return string.Format("{0:N3}", nb);
+        }
+        else if (nb > 9999)
+        {
+            nb /= 1000;
+            return ("0" + string.Format("{0:N2}", nb));
+        }
+        else if (nb > 999)
+        {
+            nb /= 1000;
+            return ("00" + string.Format("{0:N1}", nb));
+        }
+        else
+        {
+            if (nb > 99)
+             return ("000." + nb.ToString());
+            else if (nb > 9)
+                return ("000.0" + nb.ToString());
+            else
+                return ("000.00" + nb.ToString());
+        }
     }
 
     private void Update()
     {
+        float translation = Input.GetAxisRaw("Vertical");
+
         if (manager.GetKeyDown(GameManager.e_input.CANCEL) || Input.GetKeyDown("joystick button 1"))
         {
             AkSoundEngine.PostEvent("Main_Menu_UI_Validate", music);
             StartCoroutine(LoadYourAsyncScene("Menu/MainMenu"));
         }
 
+        if (manager.GetKeyDown(GameManager.e_input.DOWN) || (translation < -0.8))
+        {
+            scrollbar.value -= 0.01f;
+        }
+        else if (manager.GetKeyDown(GameManager.e_input.UP) || (translation > 0.8))
+        {
+            scrollbar.value += 0.01f;
+        }
     }
 }
