51f43962c3edd19c89dc8c3ae29ad04aff2eeea5
nicolas.noel <nico@krismas.net>
2018-01-23 15:28:09 +0100

    Implémentation vagues tests supplémentaires + Script GD test gameover temp

---
+++ CircleShmup/Assets/Resources/Prefabs/GD/GD_Scripts/Game_Over_Test.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game_Over_Test : MonoBehaviour {
+
+	public Transform Player;
+	private int hitpoints = 5;
+	private Rect monrect = new Rect (0,0,100,100);
+	private string vie = "";
+	private GUIStyle guiStyle = new GUIStyle();
+
+	// Use this for initialization
+	void Start () {
+		
+		guiStyle.fontSize = 200; //change the font size
+		guiStyle.normal.textColor = Color.white;
+		hitpoints = GetComponent<PlayerController> ().hitPoint;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		hitpoints = GetComponent<PlayerController> ().hitPoint;
+		vie = ("" + hitpoints);
+		Debug.Log (hitpoints);
+
+		if (hitpoints <= 0)
+			SceneManager.LoadScene (SceneManager.GetActiveScene().name);
+	}
+
+	void OnGUI () {
+		GUI.Label(monrect, vie, guiStyle);
+	}
+}
