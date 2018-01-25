442293619c8e0a2f2fce46176dd6ce5e4bd78cc4
Aredhele <vincentstehly@hotmail.fr>
2018-01-25 17:20:25 +0100

    Add player hit points animator hooks

--- CircleShmup/Assets/Scripts/Controllers/Player/PlayerController.cs
+++ CircleShmup/Assets/Scripts/Controllers/Player/PlayerController.cs
@@ -161,6 +161,18 @@ public class PlayerController : Entity
     public override void OnHit(int hitPoint)
     {
         AkSoundEngine.PostEvent("Beurre_Hit", musicPlayer);
+
+        // Player life hook
+        switch(hitPoint)
+        {
+            case 0:  break;
+            case 1:  break;
+            case 2:  break;
+            case 3:  break;
+            case 4:  break;
+            case 5:  break;
+            default: break;
+        }
     }
 
     /**
